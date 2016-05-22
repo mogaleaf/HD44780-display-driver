2fd472a3498a6b3afc2827f4e41fb4443f261b67
Cecile <cecile.gomes@mogaleaf.com>
2016-05-22 21:52:12 +0200

    Ajout de display

--- include/drivers/hd44780.h
+++ include/drivers/hd44780.h
@@ -15,7 +15,7 @@ template <typename RS, typename ENABLE, typename D4, typename D5, typename D6, t
 			D6::setOutput();
 			D7::setOutput();
 			
-			os_delay_us(50000);
+			os_delay_us(650000);
 			
 			RS::clear();
 			ENABLE::clear();
@@ -33,15 +33,14 @@ template <typename RS, typename ENABLE, typename D4, typename D5, typename D6, t
 			uint8_t functionSet = 0;
 			if (lineNumber > 1)
 			{
-				functionSet |= static_cast<uint8_t>(Display_Function::D_TWO_LINES);
+				functionSet |=Display_Function::D_TWO_LINES;
 			}
 			if ((lineNumber == 1) && (dotsize > 10))
 			{
-				functionSet |= static_cast<uint8_t>(Display_Function::D_MATRICE_5_11);
+				functionSet |=Display_Function::D_MATRICE_5_11;
 			}
 			IssueCommand(Mode::FUNCTION_SET, functionSet);
-			Clear();
-			SetDisplay(true, true, true);
+         
 		}
 		
 		static void SetDisplay(bool display, bool visibleCursor, bool blinkCursor)
@@ -49,15 +48,15 @@ template <typename RS, typename ENABLE, typename D4, typename D5, typename D6, t
 			uint8_t args = 0;
 			if (display)
 			{
-				args |= static_cast<uint8_t>(Display_Control::DISPLAY_ON);
+				args |= Display_Control::DISPLAY_ON;
 			}
 			if (visibleCursor)
 			{
-				args |= static_cast<uint8_t>(Display_Control::CURSOR_VISIBLE);
+				args |= Display_Control::CURSOR_VISIBLE;
 			}
 			if (blinkCursor)
 			{
-				args |= static_cast<uint8_t>(Display_Control::BLINK_CURSOR);
+				args |= Display_Control::BLINK_CURSOR;
 			}
 			IssueCommand(Commands::DISPLAY_CONTROL, args);
 		}
@@ -66,10 +65,26 @@ template <typename RS, typename ENABLE, typename D4, typename D5, typename D6, t
 			IssueCommand(Commands::CLEAR_DISPLAY);
 			os_delay_us(2000);
 		}
-		
+
+        static void SetDirection(bool writeRight,bool shiftAdress) {
+            uint8_t args = 0;
+            if (writeRight) {
+                args |= Display_Mode::DIRECTION;
+            }
+            if (shiftAdress) {
+                args |=Display_Mode::SHIFT;
+            }
+           
+            IssueCommand(Commands::ENTRY_MODE_SET, args);
+        }
+
+        static void DisplayDigit(char character) {
+            RS::set();
+            Write(character);
+        }
 		
 	private:
-		enum class Commands:uint8_t {
+		enum Commands:uint8_t {
 			CLEAR_DISPLAY   = 0x01,
 			RETURN_HOME     = 0x02,
 			ENTRY_MODE_SET  = 0x04,
@@ -77,13 +92,13 @@ template <typename RS, typename ENABLE, typename D4, typename D5, typename D6, t
 			
 		};
 		
-		enum class Mode:uint8_t {
+		enum Mode:uint8_t {
 			FUNCTION_SET    = 0x20,
 			SET_CGRAMADDR   = 0x40,
 			SET_DDRAMADDR	= 0x80
 		};
 		
-		enum class Display_Function:uint8_t
+		enum Display_Function:uint8_t
 		{
 			D_8_BITS = 0x10,
 			D_TWO_LINES = 0x08,
@@ -91,24 +106,33 @@ template <typename RS, typename ENABLE, typename D4, typename D5, typename D6, t
 			
 		};
 		
-		enum class Display_Control:uint8_t
+		enum Display_Control:uint8_t
 		{
 			DISPLAY_ON     = 0x04,
 			CURSOR_VISIBLE = 0x02,
 			BLINK_CURSOR   = 0x01
 			
 		};
+
+        enum Display_Mode :uint8_t
+        {
+            DIRECTION = 0x02,
+            SHIFT = 0x01
+        };
 		
 		static void IssueCommand(Mode mode, uint8_t arg) {
-			Write(static_cast<uint8_t>(mode) | arg);
+            RS::clear();
+			Write(mode | arg);
 		}
 		
 		static void IssueCommand(Commands command) {
-			Write(static_cast<uint8_t>(command));
+            RS::clear();
+			Write(command);
 		}
 		
 		static void IssueCommand(Commands command,uint8_t arg) {
-			Write(static_cast<uint8_t>(command) | arg);
+            RS::clear();
+			Write(command | arg);
 		}
 
 		static void Write(uint8_t data) {
@@ -116,14 +140,14 @@ template <typename RS, typename ENABLE, typename D4, typename D5, typename D6, t
 			D5::set(((1 << 5)&data) != 0);
 			D6::set(((1 << 6)&data) != 0);
 			D7::set(((1 << 7)&data) != 0);
-			ENABLE::pulseHigh();
-			//PulseEnable();
+			//ENABLE::pulseHigh();
+			PulseEnable();
 			D4::set(((1 << 0)&data) != 0);
 			D5::set(((1 << 1)&data) != 0);
 			D6::set(((1 << 2)&data) != 0);
 			D7::set(((1 << 3)&data) != 0);
-			ENABLE::pulseHigh();
-			//PulseEnable();
+			//ENABLE::pulseHigh();
+			PulseEnable();
 		}
 		
 		static void PulseEnable() {
@@ -135,3 +159,13 @@ template <typename RS, typename ENABLE, typename D4, typename D5, typename D6, t
 			os_delay_us(100);
 		}
 	};
+
+    template <typename HD44780>
+    class HD44780Printer {
+    public:
+        static void print(const char str[]) {
+            for (auto i = 0; str[i] != '\0'; i++) {
+                HD44780::DisplayDigit(str[i]);
+            }
+        }
+    };
